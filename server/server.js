e0bf1d979d171b6f27bad1c336a266a6ede8b64d
Vaughn Walters <vaughnwalters@gmail.com>
2016-10-21 12:13:24 -0500

    pull back from DB

--- server/server.js
+++ server/server.js
@@ -15,9 +15,9 @@ app.use(json());
 
 
 // GETS ALL THE MESSAGES BACK FROM DB
-app.get('/api/getAll  ', (req, res) =>
+app.get('/api/getAll', (req, res) =>
   res.json({
-    messages: [
+    songs: [
       {
         userId: 1,
         title: 'santa2000',
@@ -40,21 +40,57 @@ app.get('/api/getAll  ', (req, res) =>
   })
 )   
 
+
 // SET UP MODEL FOR WHAT IS TO BE SENT TO DB ON FORM SUBMIT
 const Song = mongoose.model('song', {
-  userId: String,
+  userId: Number,
   title: String,
   lyric: String,
   audio: String
 })
 
+app.get('/api/songs', (req, res, err) =>
+  Song
+    .find()
+    .then(songs => res.json({ songs }))
+    .catch(err)
+) 
+
+// app.get('/api/getAll/getOne/:id', (req, res) =>
+//   res.json({
+//     messages: [
+//       {
+//         userId: 1,
+//         title: 'santa2000',
+//         lyric: 'christmas time is here.  time for beer and cheer.',
+//         audio: 'santa2000jam'
+//       },
+//       {
+//         userId: 1,
+//         title: 'Barf-o-rama',
+//         lyric: 'ate a dank noodle.  puked on a poodle',
+//         audio: 'barf-o-ramajam'
+//       },
+//       {
+//         userId: 1,
+//         title: 'jim',
+//         lyric: 'dont eat that meat stick, snap into a new trick',
+//         audio: 'barf-o-ramajam'
+//       },
+//     ],
+//   })
+// )  
+
+
+
 // POSTS NEW SONG TO DB
-app.post('/api/newSong', (req, res, err) =>
+app.post('/api/newSong', (req, res, err) => {
+  console.log("req.body", req.body);
   Song
     .create(req.body)
-    .then(msg => res.json(msg))
+    .then(song => res.json(song))
     .catch(err)
-)                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                      
+})                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                      
 
 mongoose.Promise = Promise;
 
