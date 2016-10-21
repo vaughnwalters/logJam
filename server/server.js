31ab7d3404122e79f73fd6eec5eda9c247aa4455
Vaughn Walters <vaughnwalters@gmail.com>
2016-10-21 13:17:45 -0500

    returning just one item from collection

--- server/server.js
+++ server/server.js
@@ -7,81 +7,38 @@ const mongoose = require('mongoose');
 const { json } = require('body-parser');
 
 const PORT = process.env.PORT || 3000;
-const MONGODB_URL = process.env.MONGODB_URL || 'mongodb://localhost:27017/logjam'
+const MONGODB_URL = process.env.MONGODB_URL || 'mongodb://localhost:27017/logjam';
 
 
 app.use(express.static('client'));
 app.use(json());
 
 
-// GETS ALL THE MESSAGES BACK FROM DB
-app.get('/api/getAll', (req, res) =>
-  res.json({
-    songs: [
-      {
-        userId: 1,
-        title: 'santa2000',
-        lyric: 'christmas time is here.  time for beer and cheer.',
-        audio: 'santa2000jam'
-      },
-      {
-        userId: 1,
-        title: 'Barf-o-rama',
-        lyric: 'ate a dank noodle.  puked on a poodle',
-        audio: 'barf-o-ramajam'
-      },
-      {
-        userId: 1,
-        title: 'jim',
-        lyric: 'dont eat that meat stick, snap into a new trick',
-        audio: 'barf-o-ramajam'
-      },
-    ],
-  })
-)   
-
-
 // SET UP MODEL FOR WHAT IS TO BE SENT TO DB ON FORM SUBMIT
 const Song = mongoose.model('song', {
-  userId: Number,
+  userId: String,
   title: String,
   lyric: String,
   audio: String
-})
+});
 
-app.get('/api/songs', (req, res, err) =>
+// GETS ALL THE SONGS BACK FROM THE DB
+app.get('/api/getAll', (req, res, err) => {
   Song
     .find()
     .then(songs => res.json({ songs }))
     .catch(err)
-) 
+}); 
 
-// app.get('/api/getAll/getOne/:id', (req, res) =>
-//   res.json({
-//     messages: [
-//       {
-//         userId: 1,
-//         title: 'santa2000',
-//         lyric: 'christmas time is here.  time for beer and cheer.',
-//         audio: 'santa2000jam'
-//       },
-//       {
-//         userId: 1,
-//         title: 'Barf-o-rama',
-//         lyric: 'ate a dank noodle.  puked on a poodle',
-//         audio: 'barf-o-ramajam'
-//       },
-//       {
-//         userId: 1,
-//         title: 'jim',
-//         lyric: 'dont eat that meat stick, snap into a new trick',
-//         audio: 'barf-o-ramajam'
-//       },
-//     ],
-//   })
-// )  
-
-
+// FIND ONE SONG BY ID AND RETURN THAT SONG
+app.get('/api/getOne/:id', (req, res) => {
+  console.log("req.params.id", req.params.id);
+  let songId = req.params.id;
+  Song
+    .findById(songId)
+    .then(songs => res.json({ songs }))
+});
+ 
 
 // POSTS NEW SONG TO DB
 app.post('/api/newSong', (req, res, err) => {
@@ -90,7 +47,7 @@ app.post('/api/newSong', (req, res, err) => {
     .create(req.body)
     .then(song => res.json(song))
     .catch(err)
-})                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                      
+});                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                    
 
 mongoose.Promise = Promise;
 
