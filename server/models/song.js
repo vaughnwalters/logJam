1826bc972e419da164fa81b1081d91045242b217
Vaughn Walters <vaughnwalters@gmail.com>
2016-10-24 10:21:04 -0500

    added models

---
+++ server/models/song.js
@@ -0,0 +1,12 @@
+'use strict'
+
+const mongoose = require('mongoose')
+
+
+// SET UP MODEL FOR WHAT IS TO BE SENT TO DB ON FORM SUBMIT
+module.exports = mongoose.model('Song', {
+  userId: String,
+  title: String,
+  lyric: String,
+  audio: String
+});
