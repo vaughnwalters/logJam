58e72531d578a8ede29b8a858052c33313ee6b76
Vaughn Walters <vaughnwalters@gmail.com>
2016-10-26 11:07:42 -0500

    added bower as dependency

--- client/app/app.js
+++ client/app/app.js
@@ -1,1 +1,10 @@
-"use strict";
+'use strict';
+
+const app = angular.module('logjam', ['ngRoute'])
+  .config($routeProvider =>
+    $routeProvider
+      .when('/', {
+        controller: 'MainCtrl',
+        templateUrl: 'partials/main.html'
+      })
+  )
