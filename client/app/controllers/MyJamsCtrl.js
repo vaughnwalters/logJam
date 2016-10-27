e12c08f1ce062df31caf4f09b178d7d20f21cb1b
Vaughn Walters <vaughnwalters@gmail.com>
2016-10-27 11:36:09 -0500

    song array returning to DOM on myJams partial

--- client/app/controllers/MyJamsCtrl.js
+++ client/app/controllers/MyJamsCtrl.js
@@ -2,8 +2,30 @@
 
 console.log("MyJamsCtrl.js loading");
 
-app.controller("MyJamsCtrl", function ($scope, $routeParams, $location) {
+app.controller("MyJamsCtrl", function ($scope, $routeParams, $location, DatabaseFactory) {
   // inject factories into the scope as needed
+
+  $scope.allUserJams = [];
+
+  $scope.getAllSongs = function(userId) {
+    console.log("getAllSongs userId", userId);
+    DatabaseFactory.getUserJamsFromDb(userId)
+      .then(function(allUserJams) {
+        $scope.allUserJams = allUserJams.data.userSongArr;
+        console.log("allUserJams", $scope.allUserJams);
+      })
+  }
+
+
 
 });
 
+
+  // $scope.getSong = function(songId) {
+  //   console.log("getSong");
+  //   DatabaseFactory.getJamFromDb(songId)
+  //     .then(function(jam) {
+  //       console.log("jam.data", jam.data.songs);
+  //       $scope.jam = jam.data.songs;
+  //   });
+  // }    
--- client/app/factories/DatabaseFactory.js
+++ client/app/factories/DatabaseFactory.js
@@ -6,26 +6,38 @@ app.factory("DatabaseFactory", function(LogJamUrl, $q, $http) {
 
   let getJamFromDb = function(jamId) {
     console.log("LogJamUrl", LogJamUrl);
-    console.log("getJamFromDb");
     console.log("jamId", jamId);
 
-    
-    // return $q(function(resolve, reject) {
-    //   $http.get(`${LogJamUrl}/api/getOne/${jamId}`)
-    //   .then(function(jamObj) {
-    //     console.log("jamObj", jamObj);
-    //     resolve (jamObj);
-    //   })
-    //   .catch(function(error) {
-    //     reject(error);
-    //   });
-    // });
-// same as above
     return $http.get(`${LogJamUrl}/api/getOne/${jamId}`)
   };
 
+
+
+  let getUserJamsFromDb = function(userId) {
+    console.log("userId", userId );
+    return  $http.get(`${LogJamUrl}/api/getAll/${userId}`)
+  }
+
+
+  // let getUserJamsFromDb = function(userId) {
+  //   return $q(function(resolve, reject) {
+  //     $http.get(`${LogJamUrl}/api/getOne/${jamId}`)
+  //     .then(function(jamObj) {
+  //       console.log("jamObj", jamObj);
+  //       resolve (jamObj);
+  //     })
+  //     .catch(function(error) {
+  //       reject(error);
+  //     });
+  //   });
+  // }
+
+
+
+
+
 // don't forget to return any functions created in this factory
-  return {getJamFromDb};
+  return {getJamFromDb, getUserJamsFromDb};
   
 });
 
