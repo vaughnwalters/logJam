3b8684da0212823910f7d7ef01c36d1fc971fa9e
Vaughn Walters <vaughnwalters@gmail.com>
2016-10-27 12:22:11 -0500

    jams clickable to bring back data from individual jam and display in DOM

--- client/app/controllers/ViewJamCtrl.js
+++ client/app/controllers/ViewJamCtrl.js
@@ -5,12 +5,12 @@ console.log("ViewJamCtrl.js loading");
 app.controller("ViewJamCtrl", function ($scope, $routeParams, $location, DatabaseFactory) {
   // inject factories into the scope as needed
 
-  $scope.getSong = function(songId) {
-    console.log("getSong");
-    DatabaseFactory.getJamFromDb(songId)
-      .then(function(jam) {
-        console.log("jam.data", jam.data.songs);
-        $scope.jam = jam.data.songs;
-    });
-  }    
+  $scope.songId = $routeParams.id
+
+  DatabaseFactory.getJamFromDb($scope.songId)
+    .then(function(jam) {
+      console.log("jam.data", jam.data.songs);
+      $scope.jam = jam.data.songs;
+  });
+
 });
