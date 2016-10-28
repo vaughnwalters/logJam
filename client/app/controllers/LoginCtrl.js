204d38282927e624679433b8b6322390dffc9c87
Vaughn Walters <vaughnwalters@gmail.com>
2016-10-28 11:30:35 -0500

    working on login

--- client/app/controllers/LoginCtrl.js
+++ client/app/controllers/LoginCtrl.js
@@ -2,8 +2,39 @@
 
 console.log("LoginCtrl.js loading");
 
-app.controller("LoginCtrl", function ($scope, $routeParams, $location) {
+app.controller("LoginCtrl", function ($scope, $routeParams, $location, AuthFactory) {
   // inject factories into the scope as needed
+
+  let user = {};
+
+  $scope.login = () => {
+    user = {
+      email: $scope.email,
+      password: $scope.password
+    }
+    AuthFactory.loginUser(user)
+    .then((userObj) => {
+      console.log("userObj", userObj);
+      $location.path('/profile')
+    })
+
+  
+  }
 
 });
 
+
+//   $scope.saveNewJam = () => {
+//     newJam = {
+//       userId: userId,
+//       title: $scope.title,
+//       lyric: $scope.lyric
+//     }
+//     console.log("newJam", newJam);
+//     DatabaseFactory.postNewJam(newJam)
+//     .then((newJam) => { 
+//       console.log("newJam", newJam);
+//       // $location.path('/myJams')
+//     })
+//   }
+// });
