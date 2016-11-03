45d0200ca193641e8eea58b2d58c2c4b7c3c18be
Vaughn Walters <vaughnwalters@gmail.com>
2016-11-03 09:57:05 -0500

    removed materialize underlines

--- client/app/app.js
+++ client/app/app.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const app = angular.module('logjam', ['ngRoute', 'angularAudioRecorder', 'ui.materialize']) 
+const app = angular.module('logjam', ['ngRoute', 'angularAudioRecorder']) 
   // .constant('LogJamUrl', "http://localhost:3000")
   // .constant('LogJamUrl', "https://vw-logjam.herokuapp.com")
 
